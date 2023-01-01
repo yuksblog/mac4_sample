510fcd334b01ecd75e8a848fe5e73d1c7218c78d
yuksblog <kotatu.kotatu@gmail.com>
2023-01-01 14:44:35 +0900

    Initial Commit

---
+++ Mac4Sample/Mac4SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac4SampleApp.swift
+//  Mac4Sample
+//
+//  Created by yuksblog on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct Mac4SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
